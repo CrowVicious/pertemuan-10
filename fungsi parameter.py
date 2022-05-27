3b638921f73355dbe9918589073d523e0dbf7520
tedo <tedoar400@gmail.com>
2022-05-27 15:48:59 +0700

    percobaan def

---
+++ fungsi parameter.py
@@ -0,0 +1,12 @@
+# def selamat_datang(nama):
+#     print("halo",nama,"apa kabar")
+
+# selamat_datang("anda siapa")
+
+def greattings(nama,waktu):
+    print("halo",nama,"selamat",waktu,)
+    print("apakah",nama,"sudah datang",waktu,"ini")
+
+greattings("andi","pagi")
+greattings("wahyu","siang")
+greattings("rudi","malam")
